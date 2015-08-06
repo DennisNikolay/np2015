c03e0c33239bc9447bdc84e0f1a3093ee50f3260
Mo <mo_1993@web.de>
2015-08-06 15:27:34 +0200

    observer

--- src/impl/GlobalObserver.java
+++ src/impl/GlobalObserver.java
@@ -1,7 +1,25 @@
 package impl;
 
+import java.util.Observable;
 import java.util.Observer;
-//TODO: Fill out this!
-public interface GlobalObserver extends Observer {
 
+
+public class GlobalObserver implements Observer {
+
+	/**
+	 * The counter which represents the threads which are already terminated because of local convergence.
+	 */
+	int alreadyTerminated = 0;
+	
+	@Override
+	public void update(Observable o, Object arg) {
+		// TODO Auto-generated method stub
+		
+		
+	}
+	
+	private checkGlobalConvergence() {
+		
+	}
+	
 }
