5620c81de4105a949d854ceabd8749fcc2359619
Mo <mo_1993@web.de>
2015-08-12 17:57:40 +0200

    try finally

--- src/np2015/NPOsmose.java
+++ src/np2015/NPOsmose.java
@@ -15,49 +15,60 @@ import com.google.gson.Gson;
 public class NPOsmose {
 
 	public static GraphInfo ginfo;
-	public static GlobalObserver o=new GlobalObserver();
+	public static GlobalObserver o = new GlobalObserver();
 	public static double epsilon;
-	
+
 	public static int workersActive = 0;
-	
-	public static Lock lock=new ReentrantLock();
-	public static Condition condition=lock.newCondition();
-	
-	
-	public static void main(String[] args) throws IOException, InterruptedException {
+
+	public static Lock lock = new ReentrantLock();
+	public static Condition condition = lock.newCondition();
+
+	public static void main(String[] args) throws IOException,
+			InterruptedException {
 		Gson gson = new Gson();
 		String json = "";
 		// read data in
 		if (args.length != 0) {
 			Path path = Paths.get(args[0]);
 			try {
-				json = new String(Files.readAllBytes(path)); 
+				json = new String(Files.readAllBytes(path));
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		} else {
-			System.err.println("You must provide the serialized file as the first argument!");
+			System.err
+					.println("You must provide the serialized file as the first argument!");
 		}
 		GraphInfo ginfo = gson.fromJson(json, GraphInfo.class);
-		NPOsmose.ginfo=ginfo;
-		// Your implementation can now access ginfo to read out all important values
-		Entry<Integer, HashMap<Integer, Double>> e=ginfo.column2row2initialValue.entrySet().iterator().next();
-		ColumnWorkerImpl worker=new ColumnWorkerImpl(e.getValue(), e.getKey(), ginfo, o, null, null);
+		NPOsmose.ginfo = ginfo;
+		// Your implementation can now access ginfo to read out all important
+		// values
+		Entry<Integer, HashMap<Integer, Double>> e = ginfo.column2row2initialValue
+				.entrySet().iterator().next();
+		ColumnWorkerImpl worker = new ColumnWorkerImpl(e.getValue(),
+				e.getKey(), ginfo, o, null, null);
 		new Thread(worker).start();
 		lock.lock();
-		while(!o.allTerminated())
-			condition.await();
-		lock.unlock();
-		ImageConvertible graph = new ImageConvertibleImpl(); // <--- you should implement ImageConvertible to write the graph out
+		try {
+			while (!o.allTerminated())
+				condition.await();
+		} finally {
+			lock.unlock();
+		}
+		ImageConvertible graph = new ImageConvertibleImpl(); // <--- you should
+																// implement
+																// ImageConvertible
+																// to write the
+																// graph out
 		ginfo.write2File("./result.txt", graph);
 	}
 
 	public static synchronized void incrementWorkersActive() {
 		workersActive++;
 	}
-	
+
 	public static synchronized int getWorkersActive() {
 		return workersActive;
 	}
-	
+
 }
